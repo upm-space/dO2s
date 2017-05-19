be74ebd3fa0f93c7740951154399cf10ecdaf1a1
Daniel Oliver <de.oliver@alumnos.upm.es>
2017-05-19 13:40:21 +0200

    Comentarios en button-basic, uso de Home como zona de testeo y comienzo a crear el Widget del botón de reproducción.

--- meteor/imports/ui/pages/home.jsx
+++ meteor/imports/ui/pages/home.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { PlayConfigButton } from '../widgets/play-config-button';
 
-const Home = () => (<h3>Home</h3>);
+// const Home = () => (<h3>Home</h3>);
+// export default Home;
 
-export default Home;
+export default class Home extends Component{
+  render(){
+    return(
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <PlayConfigButton playStatus={0} playSpeed={1}/>
+      </div>
+
+      )
+    }
+  }
