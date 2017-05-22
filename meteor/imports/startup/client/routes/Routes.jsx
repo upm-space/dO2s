10b7209c6740219662b100b47ee974d8a8165f85
pilarArr <pilararr@users.noreply.github.com>
2017-05-22 12:39:07 +0200

    Formatting
    Changed some names, changed position of some code in file, added the users page and a grid, which still does work I think

--- meteor/imports/startup/client/routes/Routes.jsx
+++ meteor/imports/startup/client/routes/Routes.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Grid } from 'react-bootstrap';
 
-import App from '../../../ui/layouts/app';
+import App from '../../../ui/layouts/App';
 
-import Home from '../../../ui/pages/home';
-import One from '../../../ui/pages/one';
-import Two from '../../../ui/pages/two';
-import NotFound from '../../../ui/pages/not-found';
+import Index from '../../../ui/pages/Index';
+import One from '../../../ui/pages/One';
+import Two from '../../../ui/pages/Two';
+import NotFound from '../../../ui/pages/NotFound';
+import Users from '../../../ui/pages/Users';
 
-import UserCouta from '../../../ui/forms/user-cuota';
-import { UserManagementLayout } from '../../../ui/layouts/user-mng-layout';
+import UserCouta from '../../../ui/forms/UserCuota';
+import { UserManagementLayout } from '../../../ui/layouts/UserMngLayout';
 
+
+const UserManagementRoutes = () => (
+    <UserManagementLayout>
+        <Switch>
+            <Route path="/usrmng/user-cuota" component={UserCouta} />
+            <Route component={ NotFound } />
+        </Switch>
+
+    </UserManagementLayout>
+);
 
 const Routes = () => (
     <Router>
         <App>
-            <Switch>
-                <Route exact path="/" component={ Home }/>
-                <Route path="/usrmng" component={ UserManagementRoutes } />
-                <Route path="/one" component={ One } />
-                <Route path="/two" component={ Two } />
-                <Route component={ NotFound } />
-            </Switch>
+            <Grid>
+                <Switch>
+                    <Route exact path="/" component={ Index  }/>
+                    <Route path="/usrmng" component={ Users } />
+                    <Route path="/one" component={ One } />
+                    <Route path="/two" component={ Two } />
+                    <Route component={ NotFound } />
+                </Switch>
+            </Grid>
         </App>
     </Router>
 );
 
-const UserManagementRoutes = () => (
-    <UserManagementLayout>
-        <Route path="/usrmng/user-cuota" component={UserCouta} />
-    </UserManagementLayout>
-);
 
 
 export default Routes;
