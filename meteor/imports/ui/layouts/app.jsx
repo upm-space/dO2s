fde7fad5dcf3786120d5e25774d18a3129cfabef
pilarArr <pilararr@users.noreply.github.com>
2017-05-22 12:47:23 +0200

    Try-out to add authetication checking, still does not work.

--- meteor/imports/ui/layouts/app.jsx
+++ meteor/imports/ui/layouts/app.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import Navigation from '../components/navigation';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createContainer } from 'meteor/react-meteor-data';
+import Navigation from '../components/Navigation';
+
 
 const App = ( {children} ) => (
     <div className="App">
@@ -9,3 +12,20 @@ const App = ( {children} ) => (
 );
 
 export default App;
+
+//
+// App.propTypes = {
+//   loggingIn: PropTypes.bool,
+//   authenticated: PropTypes.bool,
+//   children: PropTypes.node,
+// };
+//
+// export default createContainer((props) => {
+//     const loggingIn = Meteor.loggingIn();
+//     const authenticated = !loggingIn && !!Meteor.userId();
+//     return {
+//         loggingIn: loggingIn,
+//         authenticated: authenticated,
+//         children: props.children,
+//     };
+// }, App);
