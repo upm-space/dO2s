6e85850e3dad0b8cfda0dbe7d7ca0407d1c79466
pilarArr <pilararr@users.noreply.github.com>
2017-05-22 12:47:36 +0200

    Adding the user Admin WIP

---
+++ meteor/imports/ui/components/User.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createContainer } from 'meteor/react-meteor-data';
+//
+// import { ItemBasic } from '../components/ItemBasic';
+// import { SearchLayout } from './SearchLayoutBasic';
+
+
+export default class User extends Component {
+    render() {
+        return (
+            <div>hello</div>
+        )
+    }
+}
