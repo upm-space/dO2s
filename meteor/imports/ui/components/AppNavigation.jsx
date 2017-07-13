6632acb55fcc9a7c802856653563f191beb601c4
pilarArr <pilararr@users.noreply.github.com>
2017-07-13 13:13:43 +0200

    Added linting and lint config file

--- meteor/imports/ui/components/AppNavigation.jsx
+++ meteor/imports/ui/components/AppNavigation.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Navbar, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -8,18 +8,18 @@ import PublicNavigation from './PublicNavigation';
 const renderNavigation = (authenticated, isAdmin) =>
 (authenticated ? <Navigation isAdmin={isAdmin} /> : <PublicNavigation />);
 
-const AppNavigation = ({authenticated, isAdmin}) => (
-    <Navbar collapseOnSelect fluid>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <LinkContainer to="/" exact>
-                    <Button bsStyle="link">dO2s</Button>
-                </LinkContainer>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        { renderNavigation(authenticated, isAdmin) }
-    </Navbar>
+const AppNavigation = ({ authenticated, isAdmin }) => (
+  <Navbar collapseOnSelect fluid>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <LinkContainer to="/" exact>
+          <Button bsStyle="link">dO2s</Button>
+        </LinkContainer>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    { renderNavigation(authenticated, isAdmin) }
+  </Navbar>
 );
 
 export default AppNavigation;
