7c4e5813ab03c03c7d6997fd16a5d33b7bcd1179
pilarArr <pilararr@users.noreply.github.com>
2017-07-13 14:57:33 +0200

    Stylesheet structure changed :art: and updated dependences :arrow_up:
    Usgin a new structure where the global stylesheets are located
    in /imports/ui/stylesheets and component stylesheets will we loaded
    at component level.(TODO)
    
    The bootstrap css is currently loaded on `index.js`, it can be
    loaded on the `app.scss` file if the extension is `.scss`.

--- meteor/imports/startup/client/index.js
+++ meteor/imports/startup/client/index.js
@@ -1,14 +1,17 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
+
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../ui/stylesheets/app.scss';
 
 import Routes from './Routes';
 
 Bert.defaults.style = 'growl-top-right';
 
 Meteor.startup(() => {
-    render(<Routes />, document.getElementById('react-root'));
+  render(<Routes />, document.getElementById('react-root'));
 });
