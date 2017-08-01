3e364582b4f73d72dea91ab69530f42fd24cd52d
pilarArr <pilararr@users.noreply.github.com>
2017-08-01 13:20:31 +0200

    Move Routes to App layout

--- meteor/imports/startup/client/index.js
+++ meteor/imports/startup/client/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
-
+import App from './../ui/layouts/App/App';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../ui/stylesheets/app.scss';
 
-import Routes from './Routes';
 
 Bert.defaults.style = 'growl-top-right';
 
 Meteor.startup(() => {
-  render(<Routes />, document.getElementById('react-root'));
+  render(<App />, document.getElementById('react-root'));
 });
