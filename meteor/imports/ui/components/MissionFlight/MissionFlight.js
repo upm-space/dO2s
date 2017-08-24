4563b78f0f48e89fc2180ef9bdc9215d2427904e
lizquierdo@srmconsulting.es <lizquierdo@srmconsulting.es>
2017-08-24 15:21:10 +0200

    :start: widget Air Speed added

--- meteor/imports/ui/components/MissionFlight/MissionFlight.js
+++ meteor/imports/ui/components/MissionFlight/MissionFlight.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WidgetAirSpeed from'../WidgetAirSpeed/WidjetAirSpeed.js'
 
-const MissionFlight = ({ mission }) => (
-  <div>
-    <h1>MissionFlight</h1>
-    <p>{JSON.stringify(mission)}</p>
-  </div>
-);
+class MissionFlight extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            speed: 10,
+        };
+    }
+
+    render(){
+        return(<div>
+            <h1>MissionFlight1</h1>
+            <p>{JSON.stringify(this.props.mission)}</p>
+            <input
+                type="number"
+                name="speed"
+                value={this.state.speed}
+                ref={speed => (this.speed = speed)}
+                onChange={() => this.setState({ speed: Number(this.speed.value) })}
+            />
+            <WidgetAirSpeed instSize="400" id="wAirSpeed" speedProp={this.state.speed}/>
+            <input type="button"   value="set speed" />
+        </div>)
+    }
+}
+
 
 MissionFlight.propTypes = {
   mission: PropTypes.object.isRequired,
