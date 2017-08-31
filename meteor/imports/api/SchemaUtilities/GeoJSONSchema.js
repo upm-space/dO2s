43509f23aa98aaa03f17cd4ede816c2cf8fdd13b
lizquierdo@srmconsulting.es <lizquierdo@srmconsulting.es>
2017-08-31 11:10:23 +0200

    :star: GeoJsonSchema.js

--- meteor/imports/api/SchemaUtilities/GeoJSONSchema.js
+++ meteor/imports/api/SchemaUtilities/GeoJSONSchema.js
@@ -1,19 +1,105 @@
 import SimpleSchema from 'simpl-schema';
 
+
 const Point = new SimpleSchema({
     type: {
-        type: Number,
-        label: 'The altitude of the flight in meters',
-        min: 0,
+        type: String,
+        label: 'The type of the feature.',
+        allowedValues: ['Point'],
     },
     coordinates: {
-        type: Number,
-        label: 'The speed of the flight in meters per second',
-        min: 0,
+        type: Array,
+        minCount: 3,
+        maxCount: 3,
+        label: 'The coordinates of the point',
     },
-    entryMargin: {
+    'coordinates.$': {
         type: Number,
-        label: 'The entry margin for the fixed wing rpa in meters',
-        min: 0,
+        label: 'The coordinates of the point',
+    },
+});
+
+const LineString = new SimpleSchema({
+    type: {
+        type: String,
+        label: 'The type of the feature.',
+        allowedValues: ['LineString'],
+    },
+    coordinates: {
+        type: Array,
+        minCount: 2,
+        label: 'The coordinates of the LineString',
+    },
+    'coordinates.$': {
+        type: Array,
+        minCount: 3,
+        maxCount: 3,
+        label: 'The coordinates of the point',
+    },
+    'coordinates.$.$': {
+        type: Number,
+        label: 'The coordinates of the point',
+    },
+});
+
+const Polygon = new SimpleSchema({
+    type: {
+        type: String,
+        label: 'The type of the feature.',
+        allowedValues: ['Polygon'],
+    },
+    coordinates: {
+        type: Array,
+        minCount: 1,
+        label: 'The coordinates of the Polygon',
+    },
+    'coordinates.$': {
+        type: Array,
+        minCount: 2,
+        label: 'The coordinates of the line',
+    },
+    'coordinates.$.$': {
+        type: Array,
+        minCount: 3,
+        maxCount: 3,
+        label: 'The coordinates of the point',
+    },
+    'coordinates.$.$.$': {
+        type: Number,
+        label: 'The coordinates of the point',
+    },
+});
+
+
+const Feature = new SimpleSchema({
+    type: {
+        type: String,
+        label: 'The type of the feature.',
+        allowedValues: ['Feature'],
+    },
+    geometry: {
+        type: SimpleSchema.oneOf(Point, LineString, Polygon),
+        label: 'The geometry of the feature',
+    },
+    properties: {
+        type: Object,
+        label: 'Extra properties for the feature',
+        blackbox: true,
+    },
+});
+
+export default FeatureCollection = new SimpleSchema({
+    type: {
+        type: String,
+        label: 'The type of the feature.',
+        allowedValues: ['FeatureCollection'],
+    },
+    features: {
+        type: Array,
+        label: 'An array of the features',
+    },
+    'features.$': {
+        type: Feature,
+        label: 'A feature object',
     },
 });
