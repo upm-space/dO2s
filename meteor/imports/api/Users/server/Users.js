4a168135fbf25f25e101c23b8d97915c181db64c
pilarArr <pilararr@users.noreply.github.com>
2017-09-08 13:34:59 +0200

    :shirt: Fix linter warnings

--- meteor/imports/api/Users/server/Users.js
+++ meteor/imports/api/Users/server/Users.js
@@ -18,20 +18,20 @@ Meteor.users.schema = new SimpleSchema({
   _id: String,
   username: {
     type: String,
-        // For accounts-password, either emails or username is required, but not both.
-        // It is OK to make this
-        // optional here because the accounts-password package does its own validation.
-        // Third-party login packages may not require either.
-        // Adjust this schema as necessary for your usage.
+    // For accounts-password, either emails or username is required, but not both.
+    // It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either.
+    // Adjust this schema as necessary for your usage.
     optional: true,
   },
   emails: {
     type: Array,
-        // For accounts-password, either emails or username is required, but not both.
-        // It is OK to make this
-        // optional here because the accounts-password package does its own validation.
-        // Third-party login packages may not require either.
-        // Adjust this schema as necessary for your usage.
+    // For accounts-password, either emails or username is required, but not both.
+    // It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either.
+    // Adjust this schema as necessary for your usage.
     optional: true,
   },
   'emails.$': {
@@ -44,13 +44,13 @@ Meteor.users.schema = new SimpleSchema({
   'emails.$.verified': {
     type: Boolean,
   },
-    // Use this registered_emails field if you are using
-    // splendido:meteor-accounts-emails-field / splendido:meteor-accounts-meld
-    // registered_emails: {
-    //     type: [Object],
-    //     optional: true,
-    //     blackbox: true
-    // },
+  // Use this registered_emails field if you are using
+  // splendido:meteor-accounts-emails-field / splendido:meteor-accounts-meld
+  // registered_emails: {
+  //     type: [Object],
+  //     optional: true,
+  //     blackbox: true
+  // },
   createdAt: {
     type: Date,
   },
@@ -59,35 +59,35 @@ Meteor.users.schema = new SimpleSchema({
     optional: true,
     blackbox: true,
   },
-    // Make sure this services field is in your schema if you're using any of the accounts packages
+  // Make sure this services field is in your schema if you're using any of the accounts packages
   services: {
     type: Object,
     optional: true,
     blackbox: true,
   },
-    // Add `roles` to your schema if you use the meteor-roles package.
-    // Option 1: Object type
-    // If you specify that type as Object, you must also specify the
-    // `Roles.GLOBAL_GROUP` group whenever you add a user to a role.
-    // Example:
-    // Roles.addUsersToRoles(userId, ["admin"], Roles.GLOBAL_GROUP);
-    // You can't mix and match adding with and without a group since
-    // you will fail validation in some cases.
-    // roles: {
-    //     type: Object,
-    //     optional: true,
-    //     blackbox: true
-    // },
-    // Option 2: [String] type
-    // If you are sure you will never need to use role groups, then
-    // you can specify [String] as the type
+  // Add `roles` to your schema if you use the meteor-roles package.
+  // Option 1: Object type
+  // If you specify that type as Object, you must also specify the
+  // `Roles.GLOBAL_GROUP` group whenever you add a user to a role.
+  // Example:
+  // Roles.addUsersToRoles(userId, ["admin"], Roles.GLOBAL_GROUP);
+  // You can't mix and match adding with and without a group since
+  // you will fail validation in some cases.
+  // roles: {
+  //     type: Object,
+  //     optional: true,
+  //     blackbox: true
+  // },
+  // Option 2: [String] type
+  // If you are sure you will never need to use role groups, then
+  // you can specify [String] as the type
   roles: {
     type: Array,
   },
   'roles.$': {
     type: String,
   },
-    // In order to avoid an 'Exception in setInterval callback' from Meteor
+  // In order to avoid an 'Exception in setInterval callback' from Meteor
   deleted: {
     type: String,
   },
