3d4de173ee3e4cad99f4b6fce2b3e6bcf12e5488
pilarArr <pilararr@users.noreply.github.com>
2017-09-14 13:01:17 +0200

    Add a function to add the altitude to the positions. WIP Add test file
    Right now it set's it all to 0.

--- meteor/imports/modules/geojson-utilities.js
+++ meteor/imports/modules/geojson-utilities.js
@@ -8,16 +8,33 @@ export const newEmptyFeaturePoint = () => ({
   },
 });
 
+export function featureSetAltitudeToZero(feature) {
+  if ((feature.geometry) && (feature.geometry.type === 'Point')) {
+    feature.geometry.coordinates[2] = 0;
+  } else if ((feature.geometry) && (feature.geometry.type === 'LineString')) {
+    for (let i = 0; i < feature.geometry.coordinates.length; i += 1) {
+      feature.geometry.coordinates[i][2] = 0;
+    }
+  } else if ((feature.geometry) && (feature.geometry.type === 'Polygon')) {
+    for (let i = 0; i < feature.geometry.coordinates.length; i += 1) {
+      for (let j = 0; j < feature.geometry.coordinates[i].length; j += 1) {
+        feature.geometry.coordinates[i][j][2] = 0;
+      }
+    }
+  }
+}
+
 export const featurePoint2latlong = featurePoint => ({
   lat: featurePoint.geometry.coordinates[1],
   lng: featurePoint.geometry.coordinates[0],
+  alt: featurePoint.geometry.coordinates[2],
 });
 
 export const latlong2featurePoint = latlong => ({
   type: 'Feature',
   geometry: {
     type: 'Point',
-    coordinates: [latlong.lng, latlong.lat, 0],
+    coordinates: [latlong.lng, latlong.lat, (latlong.alt === undefined) ? 0 : latlong.alt],
   },
 });
 
@@ -31,6 +48,10 @@ export const featurePointGetLongitude = featurePoint => (
 
 export const featurePointGetLatitude = featurePoint => (
   featurePoint.geometry && featurePoint.geometry.coordinates && featurePoint.geometry.coordinates[1]
+);
+
+export const featurePointGetAltitude = featurePoint => (
+  featurePoint.geometry && featurePoint.geometry.coordinates && featurePoint.geometry.coordinates[2]
 );
 
 export function featurePointSetZoom(featurePoint, zoom) {
@@ -47,4 +68,8 @@ export function featurePointSetLongitude(featurePoint, long) {
 
 export function featurePointSetLatitude(featurePoint, lat) {
   featurePoint.geometry.coordinates[0] = lat;
+}
+
+export function featurePointSetAltitude(featurePoint, alt) {
+  featurePoint.geometry.coordinates[2] = alt;
 }
