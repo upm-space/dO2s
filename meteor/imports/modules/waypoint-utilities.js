29e3e2afc9cd03a9831f6bece1dd2ec483471672
pilarArr <pilarArr@users.noreply.github.com>
2017-10-03 14:47:02 +0200

    :star::wrench: Add waypoint array comparison utilities

---
+++ meteor/imports/modules/waypoint-utilities.js
@@ -0,0 +1,124 @@
+import { Meteor } from 'meteor/meteor';
+
+export const setWaypointNumbers = (waypointFeatureCollection) => {
+  const waypointFeatureCollectionCopy =
+  JSON.parse(JSON.stringify(waypointFeatureCollection));
+  const waypointArray = waypointFeatureCollectionCopy.features;
+  let totalNumber = 0;
+  let webNumber = 1;
+  waypointArray.forEach((feature) => {
+    feature.properties.totalNumber = totalNumber;
+    totalNumber += 1;
+    if (feature.properties.type === 5) {
+      feature.properties.webNumber = webNumber;
+      webNumber += 1;
+    }
+  });
+  return waypointFeatureCollectionCopy;
+};
+
+export const getOperationType = (currentRPAPath, newRPAPath) => {
+  const oldWaypointCount = currentRPAPath.geometry.coordinates.length;
+  const newWaypointCount = newRPAPath.geometry.coordinates.length;
+  if (oldWaypointCount === newWaypointCount) {
+    return 'move';
+  } else if (oldWaypointCount < newWaypointCount) {
+    return 'delete';
+  } else if (oldWaypointCount > newWaypointCount) {
+    return 'add';
+  }
+};
+
+const arrayEquals = (array1, array2) => {
+  // if the other array is a falsy value, return
+  if (!array2) { return false; }
+
+  // compare lengths - can save a lot of time
+  if (array1.length !== array2.length) { return false; }
+
+  for (let i = 0; i < array1.length; i += 1) {
+    // Check if we have nested arrays
+    if (array1[i] instanceof Array && array2[i] instanceof Array) {
+      // recurse into the nested arrays
+      if (!arrayEquals(array1[i], array2[i])) { return false; }
+    } else if (array1[i] !== array2[i]) {
+      // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      return false;
+    }
+  }
+  return true;
+};
+
+const arrayContains = (item, array) => {
+  // if the other array is a falsy value, return false
+  if (!array) { return false; }
+
+  // start by assuming the array doesn't contain the thing
+  let result = false;
+  for (let i = 0; i < array.length; i += 1) {
+    // if anything in the array is the thing then change our mind from before
+    if (array[i] instanceof Array) {
+      if (arrayEquals(item, array[i])) { result = true; }
+    } else if (array[i] === item) { result = true; }
+  }
+  // return the decision we left in the variable, result
+  return result;
+};
+
+export const insertNewWaypoint = (oldWaypointList, newRPAPath) => {
+  const newWaypointList = JSON.parse(JSON.stringify(oldWaypointList));
+  const waypointArray = oldWaypointList.features;
+  const rpaArray = newRPAPath.geometry.coordinates[0];
+  for (let i = 0; rpaArray.lentgh < i; i += 1) {
+    const currentWaypointProperties = waypointArray[i].properties;
+    const currentRPAPosition = rpaArray[i].geometry.coordinates[0];
+    if (!arrayContains(currentRPAPosition, waypointArray)) {
+      const newWaypoint = {
+        type: 'Feature',
+        properties: {
+          altRelative: currentWaypointProperties.altRelative,
+          altAbsolute: currentWaypointProperties.altAbsolute,
+          altGround: currentWaypointProperties.altGround,
+          type: 5,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: currentRPAPosition,
+        },
+      };
+      newWaypointList.features.splice(i, 0, newWaypoint);
+      break;
+    }
+  }
+  return newWaypointList;
+};
+
+export const removeWaypoint = (oldWaypointList, newRPAPath) => {
+  const newWaypointList = JSON.parse(JSON.stringify(oldWaypointList));
+  const waypointArray = oldWaypointList.features;
+  const rpaArray = newRPAPath.geometry.coordinates[0];
+  for (let i = 0; waypointArray.lentgh < i; i += 1) {
+    const currentWaypointPosition = waypointArray[i].geometry.coordinates;
+    if (!arrayContains(currentWaypointPosition, rpaArray)) {
+      newWaypointList.features.splice(i, 1);
+      break;
+    }
+  }
+  return newWaypointList;
+};
+
+export const moveWaypoint = (oldWaypointList, newRPAPath) => {
+  const newWaypointList = JSON.parse(JSON.stringify(oldWaypointList));
+  const waypointArray = oldWaypointList.features;
+  const rpaArray = newRPAPath.geometry.coordinates[0];
+  for (let i = 0; rpaArray.lentgh < i; i += 1) {
+    const currentWaypointPosition = waypointArray[i].geometry.coordinates;
+    const currentRPAPosition = rpaArray[i].geometry.coordinates[0];
+    if (!(currentWaypointPosition[0] === currentRPAPosition[0] &&
+      currentWaypointPosition[1] === currentRPAPosition[1])) {
+      newWaypointList.features[i].geometry.coordinates = currentRPAPosition;
+      break;
+    }
+  }
+  return newWaypointList;
+};
