323f8e85a830af09035d7aeefbd81f49cef10f63
pilarArr <pilarArr@users.noreply.github.com>
2017-11-01 11:27:22 +0100

    Add leaflet icon path to index (DRY)

--- meteor/imports/startup/client/index.js
+++ meteor/imports/startup/client/index.js
@@ -6,11 +6,16 @@ import { Meteor } from 'meteor/meteor';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
 
 import App from '../../ui/layouts/App/App';
 import '../../ui/stylesheets/app.scss';
+
+if (Meteor.isClient) {
+  L.Icon.Default.imagePath = '/images/';
+}
 
 Bert.defaults.style = 'growl-top-right';
 
