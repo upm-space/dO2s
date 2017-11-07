ebbdd74f54a6b03e47e71cb407c78bfb1d450ff7
Héctor Alcoceba <halcoceba@gmail.com>
2017-11-07 13:57:28 +0100

    :wrench: Minor code fixes

--- meteor/imports/api/FlieTransferUtilities/server/methods.js
+++ meteor/imports/api/FlieTransferUtilities/server/methods.js
@@ -1,31 +1,16 @@
 import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
+// import { check } from 'meteor/check';
 import rateLimit from '../../../modules/rate-limit';
-// import fsHelper from '../../../modules/server/file-system';
 import { saveFileFrombuffer } from '../../../modules/server/file-system';
 
 const Future = Npm.require('fibers/future');
-const fs = require('fs');
 
 Meteor.methods({
-  saveLocalFiles: (path, fileArray) => {
-    console.log('Hola Mundo');
-    check(path, String);
-    const future = new Future();
-    future.return('ok');
-    // fsHelper.saveFileFrombuffer(buffer, dir, fileName, (objJson) => {
-    saveFileFrombuffer(buffer, dir, fileName, (objJson) => {
-      future.return(objJson);
-    });
-    return future.wait();
-  },
-
   saveFile: (buffer, dir, fileName) => {
     // check(buffer, String);
     // check(dir, String);
     // check(fileName, String);
     const future = new Future();
-    // fsHelper.saveFileFrombuffer(buffer, dir, fileName, (objJson) => {
     saveFileFrombuffer(buffer, dir, fileName, (objJson) => {
       future.return(objJson);
     });
@@ -35,8 +20,6 @@ Meteor.methods({
 
 rateLimit({
   methods: [
-    'prueba',
-    'saveLocalFiles',
     'saveFile',
   ],
   limit: 5,
