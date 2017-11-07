3fc935358fa861dc43befaa4c34111b1af257275
Héctor Alcoceba <halcoceba@gmail.com>
2017-11-07 09:36:36 +0100

    :poop: No further description needed

--- meteor/imports/api/FlieTransferUtilities/server/methods.js
+++ meteor/imports/api/FlieTransferUtilities/server/methods.js
@@ -5,17 +5,6 @@ import rateLimit from '../../../modules/rate-limit';
 // const fs = require('fs');
 
 Meteor.methods({
-<<<<<<< HEAD
-  saveLocalFiles: (path, fileArray) => {
-    console.log('Hola Mundo');
-    const future = new Future();
-    future.return('ok');
-    fsHelper.saveFileFrombuffer(buffer, dir, fileName, (objJson) => {
-      future.return(objJson);
-    });
-    return future.wait();
-  },
-=======
   // saveLocalFiles: (path, fileArray) => {
   //   console.log('Hola Mundo');
   //   const future = new Future();
@@ -25,7 +14,6 @@ Meteor.methods({
   //   // });
   //   return future.wait();
   // },
->>>>>>> 10d8bf705afc5034c001547269c1bde372166c2c
   prueba: function prueba() {
     console.log('Hola mundo');
     return 8;
