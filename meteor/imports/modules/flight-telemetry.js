99be8e5aa7eaef1e69ea75dd24df0564227063d6
pilarArr <pilarArr@users.noreply.github.com>
2017-11-07 12:36:06 +0100

    :wrench: Telemetry integration WORKS
    Baby steps

--- meteor/imports/modules/flight-telemetry.js
+++ meteor/imports/modules/flight-telemetry.js
@@ -9,36 +9,200 @@ let type;
 class WebSocketTelemetry extends EventEmiter {
   constructor() {
     super();
+    this.client = new WebSocket('ws://192.168.1.47:8080/ws');
     this.init();
   }
 
+  connectToServer(toPort, bauds) {
+    let message = {};
+    if (toPort && bauds) {
+      message = { type: 'connect', bauds, port: toPort };
+    } else {
+      message = { type: 'connect' };
+    }
+
+    this.client.send(JSON.stringify(message));
+  }
+
+  connectToRPA(toPort, bauds) {
+    const message = { type: 'connect', bauds, toPort };
+    this.client.send(JSON.stringify(message));
+  }
+
+  arm() {
+    const message = { type: 'arm', result: true };
+    this.client.send(JSON.stringify(message));
+  }
+
+  disArm() {
+    const message = { type: 'arm', result: false };
+    this.client.send(JSON.stringify(message));
+  }
+
+  setFlighMode(newFlightMode) {
+    const message = { type: 'setFlighMode', flightMode: newFlightMode };
+    this.client.send(JSON.stringify(message));
+  }
+
+  requestLogList() {
+    const message = { type: 'requestLogList' };
+    this.client.send(JSON.stringify(message));
+  }
+
+  requestLog(log) {
+    // const log = document.getElementById('logs').value;
+    const id = log.split(' ')[1];
+    const message = { type: 'requestLog', id };
+    this.client.send(JSON.stringify(message));
+  }
+
+  processAttitude(data) {
+    let pitchRead = data.pitch;
+    let rollRead = data.roll;
+    let yawRead = data.yaw;
+    pitchRead = (pitchRead * 180) / Math.PI;
+    rollRead = (rollRead * 180) / Math.PI;
+    yawRead = (yawRead * 180) / Math.PI;
+    this.emit('attitudeData', { pitchRead, rollRead, yawRead });
+  }
+
+  processGPS(data) {
+    let latRead = data.lat;
+    let lonRead = data.lon;
+    let altRead = data.alt;
+    const signalType = data.fix_type; // importante para el tipo de recepción (no gps, 3dFix, etc)
+    let speedRead = data.vel;
+
+    altRead /= 1000;
+    lonRead /= 10000000;
+    latRead /= 10000000;
+    speedRead /= 100;
+    this.emit('GPSData', {
+      altRead, lonRead, latRead, signalType, speedRead,
+    });
+  }
+
+  processVFR(data) {
+    const { airspeed } = data;
+    this.emit('VFRData', { airspeed });
+  }
+
+  processHeartbeat(data) {
+    let mode = 0;
+    // console.log(this.lastMessage);
+    if (data.base_mode === 81 || data.base_mode === 89) {
+      mode = 'disarmed';
+    } else if (data.custom_mode === 5 && data.base_mode === 209) {
+      mode = 'manual';
+    } else if (data.custom_mode === 10 && data.base_mode === 217) {
+      mode = 'auto';
+    } else if (data.custom_mode === 15 && data.base_mode === 217) {
+      mode = 'guided';
+    } else if (data.custom_mode === 11 && data.base_mode === 217) {
+      mode = 'rtl';
+    } else {
+      mode = `unknown ${data.base_mode}`;
+    }
+    this.emit('HeartBeat', { mode });
+  }
+
+  processSysStatus(data) {
+    const voltage = (data.voltage_battery / 1000).toFixed(1);
+    const remaining = data.battery_remaining;
+    const amper = (data.current_battery / 100).toFixed(1);
+    this.emit('BatStatus', { voltage, remaining, amper });
+  }
+
+  processCurrentWP(data) {
+    const lastWp = data.seq;
+    this.emit('currentWP', { lastWp });
+  }
+
+  processNavController(data) {
+    const distToWp = data.wp_dist;
+    this.emit('distToWp', { distToWp });
+  }
+
+  manageMavlink(data) {
+    switch (data.parameter) {
+    case 'ATTITUDE':
+      this.processAttitude(data);
+      break;
+    case 'GPS_RAW_INT':
+      // this.processGPS(data);
+      break;
+    case 'VFR_HUD':
+      // this.processVFR(data);
+      break;
+    case 'HEARTBEAT':
+      // this.processHeartbeat(data);
+      break;
+    case 'SYS_STATUS':
+      // this.processSysStatus(data);
+      break;
+    case 'MISSION_CURRENT':
+      // this.processCurrentWP(data);
+      break;
+    case 'NAV_CONTROLLER_OUTPUT':
+      // this.processNavController(data);
+      break;
+    default:
+      throw new TypeError('The messeage is empty');
+    }
+  }
+
   init() {
-    const client = new WebSocket('ws://192.168.1.47:8080/ws');
-    client.onerror = () => {
+    this.client.onerror = () => {
       console.log('Connection Error');
     };
 
-    client.onopen = () => {
+    this.client.onopen = () => {
       console.log('WebSocket Client Connected');
-      const message = { type: 'getPorts' };
-      client.send(JSON.stringify(message));
+      const message = { type: 'connect' };
+      this.client.send(JSON.stringify(message));
     };
 
-    client.onclose = () => {
+    this.client.onclose = () => {
       console.log('Client Closed');
     };
 
-    client.onmessage = (e) => {
+    this.client.onmessage = (e) => {
       try {
         const msg = JSON.parse(e.data);
-        // console.log(msg.type)
         if (msg.type) {
-          if (msg.type === 'ports') {
-            this.emit('ports', msg.ports);
+          switch (msg.type) {
+          case 'connStatus':
+            // Message Format for connStatus
+            // msg = {
+            //   type: 'connStatus',
+            //   status: true / false,
+            //   ports: { type: 'ports', ports: [{ portName: 'name' },...] },
+            // };
+            if (msg.status) {
+              this.emit('connStatus', msg);
+            } else if (msg.ports.type === 'ports') {
+              if (!(msg.ports.ports instanceof Array)) {
+                this.emit('connStatus', msg);
+                throw new TypeError("Oops, we haven't got an array of ports!");
+              } else if (msg.ports.ports.length === 0) {
+                throw new TypeError('There are no ports to connect');
+              } else if (msg.ports.ports.length === 1) {
+                this.emit('connStatus', msg);
+                console.log('array length 1');
+                this.connectToServer(msg.ports.ports[0].portName, 155200);
+              } else if (msg.ports.ports.length > 1) {
+                this.emit('connStatus', msg);
+                console.log('many ports');
+              }
+            }
+            break;
+          case 'mavlink':
+            this.manageMavlink(msg.data);
+            break;
+          default:
+            throw new TypeError('This message is not defined');
           }
-          if (msg.type === 'mavlink') {
-            // manageMavlink(msg.data);
-          }
+
           // if (msg.type == 'message') {
           //  document.getElementById('logtext').innerHTML = msg.msg;
           // }
@@ -61,146 +225,3 @@ class WebSocketTelemetry extends EventEmiter {
 }
 
 export default WebSocketTelemetry;
-
-
-const armDisarm = function (bool) {
-  /*
-        if (armdis == "Disarmed"){
-            var message = "armDisarm,arm"
-            client.send(message);
-            armdis="Armed";
-            //document.getElementById("estatus").innerHTML = armdis;
-        }else if(armdis == "Armed"){
-            var message = "armDisarm,disarm"
-            client.send(message);
-            armDis = "Disarmed";
-            //document.getElementById("estatus").innerHTML = armdis;
-        } */
-  const message = { type: 'arm', result: bool };
-  client.send(JSON.stringify(message));
-};
-
-const connect = function () {
-  const bauds = document.getElementById('Bauds').value;
-  const port = document.getElementById('AvailablePorts').value;
-  const message = { type: 'connect', bauds, port };
-  client.send(JSON.stringify(message));
-};
-
-const setFlightMode = function () {
-  const selFlightMode = document.getElementById('flightmodeselect').value;
-  const message = { type: 'setFlighMode', flightMode: selFlightMode };
-  client.send(JSON.stringify(message));
-};
-
-const requestLogList = function () {
-  // var message = "requestLogList";
-  const message = '{"type" : "requestLogList"}';
-  client.send(message);
-};
-
-const requestLog = function () {
-  const log = document.getElementById('logs').value;
-  const id = log.split(' ')[1];
-  const message = `{"type":"requestLog" ,"id":"${id}"}`;
-  client.send(message);
-};
-
-const manageMavlink = function (data) {
-  switch (data.parameter) {
-  case 'ATTITUDE':
-    processAttitude(data);
-    break;
-  case 'GPS_RAW_INT':
-    processGPS(data);
-    break;
-  case 'VFR_HUD':
-    processVFR(data);
-    break;
-  case 'HEARTBEAT':
-    processHeartbeat(data);
-    break;
-  case 'SYS_STATUS':
-    processSysStatus(data);
-    break;
-  case 'MISSION_CURRENT':
-    processCurrentWP(data);
-    break;
-  case 'NAV_CONTROLLER_OUTPUT':
-    processNavController(data);
-  default:
-  }
-};
-
-var processAttitude = function (data) {
-  let pitchRead = data.pitch;
-  let rollRead = data.roll;
-  let yawRead = data.yaw;
-  pitchRead = pitchRead * 180 / Math.PI;
-  rollRead = rollRead * 180 / Math.PI;
-  yawRead = yawRead * 180 / Math.PI;
-  document.getElementById('pitchRead').innerHTML = `pitchRead: ${pitchRead}`;
-  document.getElementById('rollRead').innerHTML = `rollRead: ${rollRead}`;
-  document.getElementById('yawRead').innerHTML = `yawRead: ${yawRead}`;
-};
-var processGPS = function (data) {
-  let latRead = data.lat;
-  let lonRead = data.lon;
-  let altRead = data.alt;
-  const signalType = data.fix_type; // importante para el tipo de recepción (no gps, 3dFix, etc)
-  let speedRead = data.vel;
-
-  altRead /= 1000;
-  lonRead /= 10000000;
-  latRead /= 10000000;
-  speedRead /= 100;
-
-  document.getElementById('altRead').innerHTML = `altRead: ${altRead}`;
-  document.getElementById('lonRead').innerHTML = `lonRead: ${lonRead}`;
-  document.getElementById('latRead').innerHTML = `latRead: ${latRead}`;
-  document.getElementById('signalType').innerHTML = `signalType: ${signalType}`;
-  document.getElementById('speedRead').innerHTML = `speedRead: ${speedRead}`;
-};
-
-var processVFR = function (data) {
-  const airspeed = data.airspeed;
-  document.getElementById('airspeed').innerHTML = `airspeed: ${airspeed}`;
-};
-
-var processHeartbeat = function (data) {
-  let mode = 0;
-  // console.log(this.lastMessage);
-  if (data.base_mode == 81 || data.base_mode == 89) {
-    mode = 'disarmed';
-  } else if (data.custom_mode == 5 && data.base_mode == 209) {
-    mode = 'manual';
-  } else if (data.custom_mode == 10 && data.base_mode == 217) {
-    mode = 'auto';
-  } else if (data.custom_mode == 15 && data.base_mode == 217) {
-    mode = 'guided';
-  } else if (data.custom_mode == 11 && data.base_mode == 217) {
-    mode = 'rtl';
-  } else {
-    mode = `unknown ${data.base_mode}`;
-  }
-  document.getElementById('mode').innerHTML = `mode: ${mode}`;
-};
-
-var processSysStatus = function (data) {
-  const voltage = (data.voltage_battery / 1000).toFixed(1);
-  const remaining = data.battery_remaining;
-  const amper = (data.current_battery / 100).toFixed(1);
-  document.getElementById('voltage').innerHTML = `voltage: ${voltage}`;
-  document.getElementById('remaining').innerHTML = `remaining: ${remaining}`;
-  document.getElementById('amper').innerHTML = `amper: ${amper}`;
-};
-
-var processCurrentWP = function (data) {
-  const lastWp = data.seq;
-  document.getElementById('lastWp').innerHTML = `lastWp: ${lastWp}`;
-};
-
-var processNavController = function (data) {
-  const distToWp = data.wp_dist;
-  document.getElementById('distToWp').innerHTML = `distToWp: ${distToWp}`;
-};
--- meteor/imports/ui/components/PreFlightConnection/PreFlightConnection.js
+++ meteor/imports/ui/components/PreFlightConnection/PreFlightConnection.js
@@ -9,18 +9,80 @@ import WidgetAttitude from '../FlightWidgets/WidgetAttitude';
 import WidgetAirSpeed from '../FlightWidgets/WidgetAirSpeed';
 
 class PreFlightConnection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connStatus: false,
+      attitudeData: {
+        pitchRead: 0,
+        rollRead: 0,
+        yawRead: 0,
+      },
+      GPSData: {
+        altRead: 0,
+        lonRead: 0,
+        latRead: 0,
+        signalType: 0,
+        speedRead: 0,
+      },
+      VFRData: {
+        airspeed: 0,
+      },
+      HeartBeat: {
+        mode: 0,
+      },
+      BatStatus: {
+        voltage: 0,
+        remaining: 0,
+        amper: 0,
+      },
+      currentWP: {
+        lastWp: 0,
+      },
+      distToWp: {
+        distToWp: 0,
+      },
+    };
+  }
+
   componentDidMount() {
-    const myMatch = this.props.match.path;
-    console.log(myMatch.split('/').pop());
     this.props.getPath(this.props.match.path.split('/').pop());
     if (!Session.get('client')) {
       const client = new WebSocketTelemetry();
       Session.set('client', client);
 
-      client.on('ports', (ports) => {
-        ports.forEach((port) => {
-          console.log(port.portName);
-        });
+      client.on('connStatus', (msg) => {
+        this.setState({ connStatus: msg.status });
+      });
+
+      client.on('attitudeData', (attData) => {
+        console.log('emited event');
+        console.log(attData);
+        this.setState({ attitudeData: attData });
+      });
+
+      client.on('GPSData', (gpsData) => {
+        this.setState({ GPSData: gpsData });
+      });
+
+      client.on('VFRData', (vfrData) => {
+        this.setState({ VFRData: vfrData });
+      });
+
+      client.on('HeartBeat', (hearbeatData) => {
+        this.setState({ HeartBeat: hearbeatData });
+      });
+
+      client.on('BatStatus', (batStatus) => {
+        this.setState({ BatStatus: batStatus });
+      });
+
+      client.on('currentWP', (wpData) => {
+        this.setState({ currentWP: wpData });
+      });
+
+      client.on('distToWp', (wpData) => {
+        this.setState({ distToWp: wpData });
       });
     }
   }
@@ -29,36 +91,58 @@ class PreFlightConnection extends Component {
     const {
       mission, project, match, history,
     } = this.props;
+    console.log('state data');
+    console.log(this.state.attitudeData);
     return (
       <div className="PreFlightConnection">
         <h4>Establish the Connection with the RPA</h4>
         <Row>
           <Col xsOffset={0} xs={12} smOffset={3} sm={6} mdOffset={3} md={6} lgOffset={3} lg={6}>
             <Button
-              bsStyle="primary"
+              bsStyle={this.state.connStatus ? 'success' : 'danger'}
               block
               bsSize="large"
             >
-          Connect
+              {this.state.connStatus ? 'Connected' : 'No Conection'}
             </Button>
           </Col>
         </Row>
         <br />
         <Row>
           <Col xs={4} sm={4} md={4} lg={4}>
-            <WidgetAttitude id="wAltimeter" pitchProp={30} rollProp={45} />
+            <WidgetAttitude
+              id="wAttitude"
+              pitchProp={this.state.attitudeData.pitchRead}
+              rollProp={this.state.attitudeData.pitchRead}
+            />
           </Col>
           <Col xs={4} sm={4} md={4} lg={4}>
-            <WidgetAirSpeed id="wAirSpeed" speedProp={10} />
+            <WidgetAirSpeed id="wAirSpeed" speedProp={this.state.VFRData.airspeed} />
           </Col>
           <Col xs={4} sm={4} md={4} lg={4}>
-            <WidgetAltimeter id="wAltimeter" altitudeProp={50} />
+            <WidgetAltimeter id="wAltimeter" altitudeProp={this.state.GPSData.altRead} />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={6} md={6} lg={6}>
+            <h5>Current Waypoint</h5>
+            <p>{this.state.currentWP.lastWp}</p>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={6}>
+            <h5>Distance to Next Waypoint</h5>
+            <p>{this.state.distToWp.distToWp}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={6} md={6} lg={6}>
+            <h5>Mode ?? Que es esto ??</h5>
+            <p>{this.state.HeartBeat.mode}</p>
           </Col>
         </Row>
         <Row>
           <Col xs={12} sm={12} md={12} lg={12}>
             <h5>Battery</h5>
-            <ProgressBar bsStyle="success" now={10} />
+            <ProgressBar bsStyle="success" now={this.state.BatStatus.voltage} />
           </Col>
         </Row>
         <Row>
