baa0d0837c93021b187759ff1d244043d730b33b
pilarArr <pilarArr@users.noreply.github.com>
2017-11-07 09:18:03 +0100

    :shirt: Fix linter warnings

--- meteor/imports/modules/server/file-system.js
+++ meteor/imports/modules/server/file-system.js
@@ -6,41 +6,14 @@
 
 const fs = require('fs');
 // let zipFolder = require('zip-folder');
-const spawn = require('child_process').spawn;
+const { spawn } = require('child_process');
 const path = require('path');
 
 // let Future = Npm.require( 'fibers/future' );
 
 /**
- * Delete files and directories in a folder. This method is syncronous
- * @param {string} path
- * @param {int} nivel de recursividad - Siempre hay que meter 0. Es la manera que tiene la función recursiva de detectar la primera iteración y así no borrar el directorio inicial.
- */
-let deletedFiles = 0;
-const numberOfFolders = 0;
-const deleteFolderRecursiveSync = function (path, root) {
-  let files = [];
-  if (fs.existsSync(path)) {
-    files = fs.readdirSync(path);
-    files.forEach((file, index) => {
-      const curPath = buildCompletePath(path, file);
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursiveSync(curPath, false);
-      } else { // delete file
-        deletedFiles++;
-        fs.unlinkSync(curPath);
-      }
-    });
-  }
-  if (!root) {
-    fs.rmdirSync(path);
-  } else {
-    return { deletedFiles, folders: numberOfFolders };
-  }
-};
-
-/**
- * Builds a complete path taking care about the last character of the @param dir. If this does not have a slash the function will add one
+ * Builds a complete path taking care about the last character of
+ * the @param dir. If this does not have a slash the function will add one
  * @param {string} dir - directory with or without last slash
  * @param {string} fileName - filename
  * @returns {string}
@@ -49,19 +22,50 @@ const deleteFolderRecursiveSync = function (path, root) {
 const buildCompletePath = (dir, fileName) => {
   const lastChar = dir.substr(dir.length - 1);
   let slash = '';
-  if (lastChar != '/') slash = '/';
+  if (lastChar !== '/') slash = '/';
   return dir + slash + fileName;
 };
+
+/**
+ * Delete files and directories in a folder. This method is syncronous
+ * @param {string} path
+ * @param {int} nivel de recursividad - Siempre hay que meter 0.
+ Es la manera que tiene la función recursiva de detectar la primera
+  iteración y así no borrar el directorio inicial.
+ */
+let deletedFiles = 0;
+const numberOfFolders = 0;
+const deleteFolderRecursiveSync = (pathDir, root) => {
+  let files = [];
+  if (fs.existsSync(pathDir)) {
+    files = fs.readdirSync(pathDir);
+    files.forEach((file) => {
+      const curPath = buildCompletePath(pathDir, file);
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursiveSync(curPath, false);
+      } else { // delete file
+        deletedFiles += 1;
+        fs.unlinkSync(curPath);
+      }
+    });
+  }
+  if (!root) {
+    fs.rmdirSync(pathDir);
+  } else {
+    return { deletedFiles, folders: numberOfFolders };
+  }
+};
+
 
 /**
  * Get all the files from a directory
  * @param {string} path
  * @returns {[]} - array of files
  */
-const getFileList = function (path, callback) {
+const getFileList = (pathDir, callback) => {
   const filesArr = [];
   // let future = new Future();
-  fs.readdir(path, (err, files) => {
+  fs.readdir(pathDir, (err, files) => {
     files.forEach((file) => {
       filesArr.push(file);
     });
@@ -73,9 +77,10 @@ const getFileList = function (path, callback) {
 
 /**
  * Get files in a directory by an extension
- * @param {string }dir
+ * @param {string } dir
  * @param {string} extension - can be upper-case or lower-case, this function is case insensitive
- * @param {function} callback - callback with one argument that returns and array of files filered by its extension
+ * @param {function} callback - callback with one argument that returns
+    and array of files filered by its extension
  */
 const getFilesInDirByExtension = (dir, extension, callback) => {
   const filesArr = [];
@@ -95,7 +100,8 @@ const getFilesInDirByExtension = (dir, extension, callback) => {
  * Get files in a directory by a prefix
  * @param {string }dir
  * @param {string} prefix - can be upper-case or lower-case, this function is case insensitive
- * @param {function} callback - callback with one argument that returns and array of files filered by its extension
+ * @param {function} callback - callback with one argument that
+   returns and array of files filered by its extension
  */
 const getFilesInDirByPrefix = (dir, prefix, callback) => {
   const filesArr = [];
@@ -131,7 +137,8 @@ const deleteFile = (dir, fileName, callback) => {
 };
 
 /**
- * Method that save a file from a buffer. This method is used for a meteor.call, saving a file from client.
+ * Method that save a file from a buffer. This
+ method is used for a meteor.call, saving a file from client.
  * This method use Future, so it does not return anything until it finish
  * @param {binary} buffer - binary content of the file
  * @param {string} dir - directory where we want to store the file
@@ -149,8 +156,8 @@ const saveFileFrombuffer = (buffer, dir, fileName, callback) => {
       }, 1000);
       // return meteorError;
     } else if (stats.isDirectory()) {
-      fs.writeFile(fullFileName, new Buffer(buffer), 'binary', (error) => {
-        if (error) {
+      fs.writeFile(fullFileName, new Buffer(buffer), 'binary', (error2) => {
+        if (error2) {
           callback({ result: 'error', dir, fileName });
           // future.return({result:'error',dir:dir,fileName:fileName});
         } else {
@@ -170,14 +177,14 @@ const saveFileFrombuffer = (buffer, dir, fileName, callback) => {
  * create a directory with a permission type 777
  * @param {string} path
  */
-const createDirectory = (path, callback) => {
+const createDirectory = (pathDir, callback) => {
   // path = missionDirectory + path;
-  console.log(`dir created at ${path}`);
-  fs.exists(path, (exists) => {
+  console.log(`dir created at ${pathDir}`);
+  fs.exists(pathDir, (exists) => {
     console.log(`bool val ${exists}`);
     if (!exists) {
-      fs.mkdir(path, '0777', () => {
-        console.log(`dir created at ${path}`);
+      fs.mkdir(pathDir, '0777', () => {
+        console.log(`dir created at ${pathDir}`);
         callback(true);
       });
     } else {
@@ -190,7 +197,8 @@ const createDirectory = (path, callback) => {
  * Zip a complete folder
  * @param {string} folderPath
  * @param {string} zipPath
- * @param {function} callback - passing a bool argument (true if everything is ok, false if an error happens)
+ * @param {function} callback - passing a bool argument
+ (true if everything is ok, false if an error happens)
  */
 const zipACompleteFolder = (folderPath, zipPath, callback) => {
   zipFolder(folderPath, zipPath, (err) => {
@@ -200,6 +208,12 @@ const zipACompleteFolder = (folderPath, zipPath, callback) => {
       callback(true);
     }
   });
+};
+
+const getFileSize = (filename) => {
+  const stats = fs.statSync(filename);
+  const fileSizeInBytes = stats.size;
+  return fileSizeInBytes;
 };
 
 const getFolderSize = (folder, callback) => {
@@ -212,11 +226,10 @@ const getFolderSize = (folder, callback) => {
   });
 };
 
-const getFolderSize_old = (path, callback) => {
-  const size = spawn('du', ['-sh', path]);
+const getFolderSize_old = (pathDir, callback) => {
+  const size = spawn('du', ['-sh', pathDir]);
   const result = {};
   let totalSize = 0;
-
 
   size.stdout.on('data', (data) => {
     result.data = data;
@@ -232,21 +245,15 @@ const getFolderSize_old = (path, callback) => {
   });
 };
 
-let getFileSize = (filename) => {
-  const stats = fs.statSync(filename);
-  const fileSizeInBytes = stats.size;
-  return fileSizeInBytes;
-};
-
-
-export { deleteFolderRecursiveSync,
+export {
+  deleteFolderRecursiveSync,
   buildCompletePath,
   getFileList,
   getFilesInDirByExtension,
   deleteFile,
   saveFileFrombuffer,
   createDirectory,
-  zipACompleteFolder,
+  // zipACompleteFolder,
   getFolderSize,
   getFileSize,
 };
