650bee94e1ab4449b0d46a0a6046082bffbac027
pilarArr <pilarArr@users.noreply.github.com>
2017-11-17 17:21:24 +0100

    Account for no meteor-settings

--- meteor/imports/startup/server/accounts/oauth.js
+++ meteor/imports/startup/server/accounts/oauth.js
@@ -1,7 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import { ServiceConfiguration } from 'meteor/service-configuration';
 
-const OAuthSettings = Meteor.settings.private.OAuth;
+
+let OAuthSettings = {
+  facebook: { appId: '', secret: '', loginStyle: 'popup' },
+  google: { clientId: '', secret: '', loginStyle: 'popup' },
+};
+
+
+if (Meteor.settings && Meteor.settings.private && Meteor.settings.private.OAuth) {
+  OAuthSettings = Meteor.settings.private.OAuth;
+}
 
 if (OAuthSettings) {
   Object.keys(OAuthSettings).forEach((service) => {
